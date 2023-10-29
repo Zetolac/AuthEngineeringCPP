444f94ee8ad367d63af331dbeabedfbddec7dab3
Zetolac <58708564+Zetolac@users.noreply.github.com>
2023-10-29 23:43:24 +0100

    1.3 Update

--- AuthEngineeringExampleC++/Auth.h
+++ AuthEngineeringExampleC++/Auth.h
@@ -3,6 +3,7 @@
 #pragma comment(lib, "Auth.lib")
 
 #define AUTH_API __declspec(dllimport)
-extern "C" AUTH_API __int64 Login(std::string & AppId, std::string & Key, std::string & AppVersion, bool KeepVerifyingTheKey);
+extern "C" AUTH_API __int64 Login(std::string AppId, std::string Key, std::string AppVersion);
 extern "C" AUTH_API std::string GetToken();
 extern "C" AUTH_API std::string GetVariable(std::string Name);
+extern "C" AUTH_API void DiscordLog(std::string Message);
