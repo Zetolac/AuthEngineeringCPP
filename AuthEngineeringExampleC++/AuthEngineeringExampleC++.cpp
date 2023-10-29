a18295116cda6fd011b3ce8b46ffc71e5b7c453b
Zetolac <58708564+Zetolac@users.noreply.github.com>
2023-10-29 23:43:06 +0100

    1.3 Update

--- AuthEngineeringExampleC++/AuthEngineeringExampleC++.cpp
+++ AuthEngineeringExampleC++/AuthEngineeringExampleC++.cpp
@@ -5,9 +5,7 @@
 std::string AppId = ""; // REPLACE with your App ID
 std::string AppToken = ""; // REPLACE with your App Token
 std::string AppVersion = ""; // REPLACE with your App Version
-int AppEncryption = 1234; // REPLACE with your App Encryption Key
-
-bool AdvancedChecks = true; // Continue to check if the key is valid after login (If the key expires, the application closes automatically)
+int AppEncryption = 0; // REPLACE with your App Encryption Key
 
 // This code is probably not enough to be protected, so please try to: encrypt your string, make your if() unrecognizable and hide the call to the authentication functions.
 // It is not recommended to print info about the key being valid or not
@@ -15,32 +13,25 @@ bool AdvancedChecks = true; // Continue to check if the key is valid after login
 int main()
 {
     std::cout << "Welcome to auth.enginnering c++ example usage:\n Please Enter Your Key -> ";
-    std::string Key; 
+    std::string Key;
     std::cin >> Key;
 
-    uintptr_t LoginResult = Login(AppId, Key, AppVersion, AdvancedChecks); // Verify if the Key is valid
-    if (!LoginResult)  {
-        std::cout << "[-] Key is not Valid//Outdated Version/Key is now registered please retry in 15seconds\n"; 
-        Sleep(1000); 
+    uintptr_t LoginResult = Login(AppId, Key, AppVersion); // Verify if the Key is valid
+    if (!LoginResult) 
         exit(0);
-    }
 
-    if (std::string((char*)(LoginResult ^ AppEncryption)) != AppToken) { // Verify App Token returned by the server
-        std::cout << "[-] Key is not Valid\n";
-        Sleep(1000);
+    if (std::string((char*)(LoginResult ^ AppEncryption)) != AppToken) // Verify App Token returned by the server
         exit(0);
-    }
 
-    if (GetToken() != AppToken) { // Verify Stored App Token (OPTIONAL)
-        std::cout << "[-] Token is not Valid\n"; 
-        Sleep(1000); 
-        exit(0);
-    }
-
+    if (GetToken() != AppToken) // Verify Stored App Token (OPTIONAL)
+       exit(0);
+    
     std::cout << "[+] Login Success\n";
 
     std::string test = GetVariable("test"); // REPLACE with the variable name you want to fetch (OPTIONAL)
-    std::cout << test << "\n";
+    std::cout << "My Varaible Test: " << test << "\n"; // (OPTIONAL)
+
+    DiscordLog("Hello! This is a test Message!"); // REPLACE with the message you wish to send to your webhook discord (OPTIONAL) config: Dashboard -> AppSettings
 
     system("pause");
 }
